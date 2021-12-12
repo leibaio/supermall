0af10a8e5c3e489307453ae8374578aff6c2710d
leibaio <leibaio@qq.com>
2021-12-12 21:15:26 +0800

    保存商品的数据结构

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -133,7 +133,12 @@
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'news': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
       }
     },
     created() {
