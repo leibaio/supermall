613a6df51057f7a9386b7fcd467790ddb936940d
leibaio <leibaio@qq.com>
2022-01-29 19:26:09 +0800

    首页开发-BackTop的显示和隐藏

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -12,6 +12,12 @@ import BScroll from 'better-scroll'
 
 export default {
   name: "Scroll",
+  props: {
+    probeType: {
+      type: Number,
+      default: 0
+    }
+  },
   data() {
     return {
       scroll: null,
@@ -19,11 +25,17 @@ export default {
     }
   },
   mounted() {
+    // 1. 创建 BScroll 对象
     this.scroll = new BScroll(this.$refs.wrapper, {
-
+      click: true,
+      probeType: this.probeType
     })
 
-    this.scroll.scrollTo(0, 0)
+    // 2. 监听滚动的位置
+    this.scroll.on('scroll', (position) => {
+      // console.log(position)
+      this.$emit('scroll', position)
+    })
   },
   methods: {
     scrollTo(x, y, time=300) {
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,7 +2,7 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
 
-    <scroll class="content" ref="scroll">
+    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll">
       <home-swiper :banners="banners"/>
       <recommend-view :recommends="recommends"/>
       <feature-view/>
@@ -13,7 +13,7 @@
       <goods-list :goods="showGoods"/>
     </scroll>
 
-    <back-top @click.native="backClick"/>
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
 
   </div>
 </template>
@@ -52,7 +52,8 @@
           'news': {page: 0, list: []},
           'sell': {page: 0, list: []}
         },
-        currentType: 'pop'
+        currentType: 'pop',
+        isShowBackTop: false
       }
     },
     computed: {
@@ -89,6 +90,9 @@
       },
       backClick() {
         this.$refs.scroll.scrollTo(0, 0, 500)
+      },
+      contentScroll(position) {
+        this.isShowBackTop = (-position.y) > 1000
       },
       /*网络请求相关方法*/
       //请求多个数据
